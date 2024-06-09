322884f4bcffa758daa4e2bd922fc1e0e7ce5a14
Alexa Rae Schreffler <alexarae@alexas-mbp.lan>
2024-06-09 19:49:36 -0400

    looking v nice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Card from '../components/Card';
 
+const data = {
+    name: "Sarah Dole",
+    username: "sarahdole",
+    avatar: "profile-thumbnail.png",
+    body: "I've been searching for high quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"
+  }
+
 export default function Page() {
+    const { name, username, avatar, body } = data;
     return (
         <div className="flex justify-center align-center h-screen">
             <Card
-                body="this is the body"
+                avatar={avatar}
+                body={body}
+                username={username}
+                name={name}
             />
         </div>
     )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,55 @@
 import React, { ReactNode } from 'react';
 
 interface CardProps {
+    avatar: string;
     body: string;
+    username: string;
+    name: string;
     width?: number;
 }
 
-const Card = ({ body, width = 340 }: CardProps) => {
+interface IdentityHeaderProps {
+    avatar: string;
+    username: string;
+    name: string;
+}
+
+const IdentityHeader = ({ avatar, username, name}: IdentityHeaderProps) => {
+    return (
+        <>
+            <div className="flex flex-row items-center space-x-3.5">
+                <div className="w-12">
+                    <img src={avatar} />
+                </div>
+                <div>
+                    <p className="text-lg font-semibold">{name}</p>
+                    <p>@{username}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const Card = ({
+    avatar,
+    body,
+    username,
+    name,
+    width = 340
+}: CardProps) => {
     return (
         <div
-            className="flex flex-col justify-center align-center m-auto p-3 space-y-8 shadow-md"
+            className="flex flex-col justify-center align-center m-auto p-5 shadow-md rounded-lg text-sm bg-white"
             style={{ width }}    
         >
-            <div>This is my card.</div>
-            <div>{body}</div>
+            <div>
+                <IdentityHeader
+                    avatar={avatar}
+                    username={username}
+                    name={name}
+                />
+            </div>
+            <div className='py-2'>{body}</div>
         </div>
     );
 }
