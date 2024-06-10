d7c134d744bd2c1d61b5cee1994a873c69396c19
Alexa Rae Schreffler <alexarae@alexas-mbp.lan>
2024-06-09 20:57:40 -0400

    update base path

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: '/GFE-testimonial-card',
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
